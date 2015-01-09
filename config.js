b422ad5c80e6e4a14ed2ba09635f4a05070ccf22
Craig Coles <craig.coles@truedigital.co.uk>
2015-01-09 15:45:38 +0000

    Ch-ch-ch-ch-Changes

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ var express = require('express');
 module.exports = function(app, io){
 
     // Set .html as the default template extension
-    app.set('view engine', 'html');
+    app.set( 'view engine', 'html' );
 
     // Initialize the ejs template engine
-    app.engine('html', require('ejs').renderFile);
+    app.engine( 'html', require('ejs').renderFile );
 
     // Tell express where it can find the templates
-    app.set('views', __dirname + '/views');
+    app.set( 'views', __dirname + '/views' );
 
     // Make the files in the public folder available to the world
-    app.use(express.static(__dirname + '/public'));
+    app.use( express.static( path.join(__dirname, '/public') ) );
 
 };
